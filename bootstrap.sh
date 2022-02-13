beee839dcdea956aa6b227f3277af1b7d64de494
Gilles Perreymond <gperreymond@gmail.com>
2022-02-13 19:34:28 +0100

    chore(vault): certificats

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,33 @@
 ACTION=$1
 SERVICE=$2
 
+# ------------------------
+# BEFORE
+# ------------------------
+
+if [ "$ACTION" = "--network-create" ]; then
+    docker network create public
+fi
+
+if [ "$ACTION" = "--gencert-ca" ]; then
+    mkdir certs
+    ./bin/cfssl gencert -initca ca.json | ./bin/cfssljson -bare certs/root-cert
+fi
+
+# ------------------------
+# ACTIONS
+# ------------------------
+
+if [ "$ACTION" = "--generate-certs" ]; then
+    vault_token=$(cat ./data/vault.json | jq -r .root_token)
+    domain=$2
+    curl -s -H "X-Vault-Token: ${vault_token}" -X POST \
+        -d '{"common_name":"traefik.docker.localhost"}' http://localhost:8200/v1/pki/issue/generate-cert-role \
+        | jq
+fi
+
 if [ "$ACTION" = "--start" ]; then
+    
     docker-compose --file atom-${SERVICE}/docker-compose.yaml up --detach --force-recreate
 fi
 
@@ -20,9 +46,9 @@ if [ "$ACTION" = "--stop-all" ]; then
     docker network rm public
 fi
 
-if [ "$ACTION" = "--network-create" ]; then
-    docker network create public
-fi
+# ------------------------
+# VAULT
+# ------------------------
 
 if [ "$ACTION" = "--vault-init" ]; then
     initialized=$(curl -s http://localhost:8200/v1/sys/init | jq -r .initialized)
@@ -46,9 +72,30 @@ if [ "$ACTION" = "--vault-unseal" ]; then
         echo "[INFO] unseal_key_2=${unseal_key_2}"
         unseal_key_3=$(cat ./data/vault.json | jq -r .unseal_keys_b64[2])
         echo "[INFO] unseal_key_3=${unseal_key_3}"
-        curl -s -X PUT -d "{\"key\":\"${unseal_key_1}\"}" -H "Content-Type: application/json" http://localhost:8200/v1/sys/unseal | jq
-        curl -s -X PUT -d "{\"key\":\"${unseal_key_2}\"}" -H "Content-Type: application/json" http://localhost:8200/v1/sys/unseal | jq
-        curl -s -X PUT -d "{\"key\":\"${unseal_key_3}\"}" -H "Content-Type: application/json" http://localhost:8200/v1/sys/unseal | jq
+        curl -s -X PUT -d '{"key":"${unseal_key_1}"}' -H "Content-Type: application/json" http://localhost:8200/v1/sys/unseal | jq
+        curl -s -X PUT -d '{"key":"${unseal_key_2}"}' -H "Content-Type: application/json" http://localhost:8200/v1/sys/unseal | jq
+        curl -s -X PUT -d '{"key":"${unseal_key_3}"}' -H "Content-Type: application/json" http://localhost:8200/v1/sys/unseal | jq
     fi
 fi
 
+if [ "$ACTION" = "--vault-enable-pki" ]; then
+    vault_token=$(cat ./data/vault.json | jq -r .root_token)
+    # enable pki service in vault under /pki
+    curl -s -H "X-Vault-Token: ${vault_token}" -X POST \
+        -d '{"type":"pki"}' http://localhost:8200/v1/sys/mounts/pki
+    # configure
+    curl -s -H "X-Vault-Token: ${vault_token}" -X POST \
+        -d '{"max_lease_ttl":"87600h"}' http://localhost:8200/v1/sys/mounts/pki/tune || true
+    curl -s -H "X-Vault-Token: ${vault_token}" -X POST \
+        -d '{"common_name":"docker.localhost","ttl":"26280h"}' http://localhost:8200/v1/pki/intermediate/generate/internal | jq -r '.data.csr' > certs/issuing-ca.csr
+    curl -s -H "X-Vault-Token: ${vault_token}" -X POST \
+        -d '{"issuing_certificates":"http://vault-server:8200/v1/pki/ca","crl_distribution_points":"http://vault-server:8200/v1/pki/crl"}' http://localhost:8200/v1/pki/config/urls || true
+    # sign certificate with root certificate
+    ./bin/cfssl sign -ca certs/root-cert.pem -ca-key certs/root-cert-key.pem -hostname docker.localhost -config ./signing-config.json certs/issuing-ca.csr | sed -E 's/cert/certificate/' > certs/issuing.pem
+    # upload signed to Vault
+    curl -s -H "X-Vault-Token: ${vault_token}" -X POST \
+        -d @certs/issuing.pem http://localhost:8200/v1/pki/intermediate/set-signed
+    # you need to create a role to be able to generate certificates for your domain
+    curl -s -H "X-Vault-Token: ${vault_token}" -X POST \
+        -d '{"allowed_domains":"docker.localhost","allow_subdomains":true,"max_ttl":"720h"}' http://localhost:8200/v1/pki/roles/generate-cert-role || true
+fi
